fbffc48893d0bc0d6c4683e1d76847691c1d9e5c
VictorGotsenko <victor.gotsenko@gmail.com>
2024-07-22 17:38:42 +0500

    First commit for lesson JUnit

---
+++ app/src/main/java/io/hexlet/SomeClass.java
@@ -0,0 +1,11 @@
+package io.hexlet;
+
+class SomeClass {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+    
+    //public static void main(String[] args) {
+      //  System.out.println(new App().getGreeting());
+    //}
+}
